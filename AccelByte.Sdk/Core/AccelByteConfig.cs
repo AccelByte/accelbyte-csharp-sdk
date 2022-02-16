e0145f8b9d6f268d3ad8586ccbed4babaf4e0b27
Rinardi Sarean <rinardi@accelbyte.net>
2022-02-16 14:40:40 +0700

    chore(core): added copyright header

--- AccelByte.Sdk/Core/AccelByteConfig.cs
+++ AccelByte.Sdk/Core/AccelByteConfig.cs
@@ -1,3 +1,7 @@
+// Copyright (c) 2022 AccelByte Inc. All Rights Reserved.
+// This is licensed software from AccelByte Inc, for limitations
+// and restrictions contact your company contract manager.
+
 using AccelByte.Sdk.Core.Repository;
 using AccelByte.Sdk.Core.Client;
 
