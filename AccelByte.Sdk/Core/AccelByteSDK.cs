3e45fc7287a5c30a5c0352405e04f144df3ff9cb
Rinardi Sarean <rinardi@accelbyte.net>
2022-02-22 08:02:48 +0700

    feat(sdk): modify login method to use builder pattern

--- AccelByte.Sdk/Core/AccelByteSDK.cs
+++ AccelByte.Sdk/Core/AccelByteSDK.cs
@@ -48,16 +48,13 @@ namespace AccelByte.Sdk.Core
             var codeChallenge = Helper.GenerateCodeChallenge(codeVerifier);
             var clientId = Configuration.ConfigRepository.ClientId;
 
-            var oAuth20 = new OAuth20(this);
-            var authorizeV3 = new AuthorizeV3(
-                    codeChallenge,
-                    "S256",
-                    null,
-                    "commerce account social publishing analytics",
-                    null,
-                    null,
-                    clientId,
-                    "code");
+            var oAuth20 = new OAuth20(this);            
+            var authorizeV3 = AuthorizeV3.Builder
+                .SetCodeChallenge(codeChallenge)
+                .SetCodeChallengeMethod("S256")
+                .SetScope("commerce account social publishing analytics")
+                .Build(clientId,"code");
+
             var authorizeV3Response = oAuth20.AuthorizeV3(authorizeV3);
 
             var authorizeV3Query = HttpUtility.ParseQueryString( new Uri(authorizeV3Response).Query);
@@ -65,30 +62,20 @@ namespace AccelByte.Sdk.Core
                     throw new Exception($"Not getting the expected value from backend (key: {authorizeV3.LocationQuery})");;
 
             var oAuth20Extension = new OAuth20Extension(this);
-            var userAuthenticationV3 = new UserAuthenticationV3(
-                    clientId,
-                    null,
-                    null,
-                    password,
-                    requestId,
-                    username);
+            var userAuthenticationV3 = UserAuthenticationV3.Builder
+                .SetClientId(clientId)
+                .Build(password,requestId,username);
             var userAuthenticationResponse = oAuth20Extension.UserAuthenticationV3(userAuthenticationV3);
 
             authorizeV3Query = HttpUtility.ParseQueryString( new Uri(userAuthenticationResponse).Query);
             var code = authorizeV3Query[userAuthenticationV3.LocationQuery] ?? 
                     throw new Exception($"Not getting the expected value from backend (key: {userAuthenticationV3.LocationQuery})");;
 
-            var tokenGrantV3 = new TokenGrantV3(
-                    clientId,
-                    code,
-                    codeVerifier,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    "authorization_code");
-                    
+            var tokenGrantV3 = TokenGrantV3.Builder
+                .SetClientId(clientId)
+                .SetCode(code)
+                .SetCodeVerifier(codeVerifier)
+                .Build("authorization_code");
             tokenGrantV3.Security = null;
 
             var token = oAuth20.TokenGrantV3(tokenGrantV3) ??
