54efe106cb3f9f80d547df8a906d172139cab146
Rinardi Sarean <rinardi@accelbyte.net>
2022-11-18 11:27:03 +0700

    test(integration): add more tests for session and match2

--- AccelByte.Sdk.Tests/Services/MatchV2Tests.cs
+++ AccelByte.Sdk.Tests/Services/MatchV2Tests.cs
@@ -3,6 +3,7 @@
 // and restrictions contact your company contract manager.
 
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using NUnit.Framework;
 
@@ -11,7 +12,8 @@ using AccelByte.Sdk.Api;
 using AccelByte.Sdk.Core.Util;
 
 using AccelByte.Sdk.Api.Match2.Model;
-
+using AccelByte.Sdk.Api.Session.Model;
+using AccelByte.Sdk.Tests.Integration;
 namespace AccelByte.Sdk.Tests.Services
 {
     [TestFixture(Category = "FluentIntegration")]
@@ -29,20 +31,143 @@ namespace AccelByte.Sdk.Tests.Services
 
             Assert.DoesNotThrow(() =>
             {
+                #region Match2 healthcheck
                 _Sdk.Match2.Operations.GetHealthcheckInfoV1Op
                     .Execute();
+                #endregion
             });
         }
 
         [Test]
-        public void ListTest()
+        [Ignore("There still are some mismatch api response")]
+        public void MatchPoolTests()
         {
             Assert.IsNotNull(_Sdk);
             if (_Sdk == null)
                 return;
 
-            var response = _Sdk.Match2.MatchFunctions.MatchFunctionListOp
+            DisableRetry();
+
+            string nameSuffix = Helper.GenerateRandomId(4);
+
+            string cfgTemplateName = "csharp_sdk_template_" + nameSuffix;
+            string rulesetName = "csharp_sdk_ruleset_" + nameSuffix;
+            string poolName = "csharp_sdk_pool_" + nameSuffix;
+
+            _Sdk.Session.ConfigurationTemplate.AdminCreateConfigurationTemplateV1Op
+                .Execute(new()
+                {
+                    Name = cfgTemplateName,
+                    Type = "P2P",
+                    MinPlayers = 2,
+                    MaxPlayers = 2,
+                    InviteTimeout = 60,
+                    InactiveTimeout = 60,
+                    Joinability = "OPEN",
+                    ClientVersion = "1.0.0",
+                    RequestedRegions = new List<string>()
+                    {
+                        "us-west-2"
+                    }
+                }, _Sdk.Namespace);
+
+            #region Create a match rule set
+            ApiMatchRuleSet cRuleSetBody = new ApiMatchRuleSet()
+            {
+                Name = rulesetName,
+                Data = "{\"alliance\":{\"minNumber\":\"2\",\"maxNumber\":\"10\",\"playerMinNumber\":\"2\",\"playerMaxNumber\":\"4\"},\"matchingRules\":[{\"attribute\":\"\",\"criteria\":\"distance\",\"reference\":\"\"}],\"flexingRules\":[{\"duration\":\"600\",\"attribute\":\"\",\"criteria\":\"distance\",\"reference\":\"\"}],\"match_options\":{\"options\":[{\"name\":\"\",\"type\":\"any\"}]},\"alliance_flexing_rule\":[{\"duration\":\"600\",\"min_number\":\"\",\"max_number\":\"\",\"player_min_number\":\"\",\"player_max_number\":\"\"}]}"
+            };
+
+            _Sdk.Match2.RuleSets.CreateRuleSetOp
+                .Execute(cRuleSetBody, _Sdk.Namespace);
+            #endregion
+
+            #region Create a match pool
+            ApiMatchPool createPoolBody = new ApiMatchPool()
+            {
+                Name = poolName,
+                MatchFunction = "basic",
+                SessionTemplate = cfgTemplateName,
+                BackfillTicketExpirationSeconds = 600,
+                RuleSet = rulesetName,
+                TicketExpirationSeconds = 600
+            };
+
+            _Sdk.Match2.MatchPools.CreateMatchPoolOp
+                .Execute(createPoolBody, _Sdk.Namespace);
+            #endregion
+
+            #region List match pools
+            ApiListMatchPoolsResponse? poolList = _Sdk.Match2.MatchPools.MatchPoolListOp
                 .Execute(_Sdk.Namespace);
+            #endregion
+            Assert.IsNotNull(poolList);
+            if (poolList != null)
+            {
+                ApiMatchPool? poolItem = poolList.Data!.Where(p => p.Name! == poolName).FirstOrDefault();
+                Assert.IsNotNull(poolItem);
+            }
+            
+            ITestPlayer player1 = new NewTestPlayer(_Sdk, true);
+            player1.Run((sdk, player) =>
+            {
+                string gameSessionId = String.Empty;
+                var newGSResponse = sdk.Session.GameSession.CreateGameSessionOp
+                    .Execute(new() { ConfigurationName = cfgTemplateName }, sdk.Namespace);
+                if (newGSResponse != null)
+                    gameSessionId = newGSResponse.Id!;
+
+                #region User create a match ticket
+                ApiMatchTicketRequest ticketRequest = new ApiMatchTicketRequest()
+                {
+                    MatchPool = poolName,
+                    SessionID = gameSessionId
+                };
+
+                ApiMatchTicketResponse? nTicketResponse = sdk.Match2.MatchTickets.CreateMatchTicketOp
+                    .Execute(ticketRequest, sdk.Namespace);
+                #endregion
+                Assert.IsNotNull(nTicketResponse);
+                string ticketId = nTicketResponse!.MatchTicketID!;
+
+                #region User delete a match ticket
+                sdk.Match2.MatchTickets.DeleteMatchTicketOp
+                    .Execute(sdk.Namespace, ticketId);
+                #endregion
+
+                sdk.Session.GameSession.DeleteGameSessionOp
+                    .Execute(sdk.Namespace, gameSessionId);
+            });
+
+            player1.Logout();
+
+            #region Delete a match pool
+            _Sdk.Match2.MatchPools.DeleteMatchPoolOp
+                .Execute(_Sdk.Namespace, poolName);
+            #endregion
+
+            #region Delete a match rule set
+            _Sdk.Match2.RuleSets.DeleteRuleSetOp
+                .Execute(_Sdk.Namespace, rulesetName);
+            #endregion
+
+            _Sdk.Session.ConfigurationTemplate.AdminDeleteConfigurationTemplateV1Op
+                .Execute(cfgTemplateName, _Sdk.Namespace);
+
+            ResetPolicy();
+        }
+
+        [Test]
+        public void MatchFunctionTests()
+        {
+            Assert.IsNotNull(_Sdk);
+            if (_Sdk == null)
+                return;
+
+            #region List match functions
+            ApiListMatchFunctionsResponse? response = _Sdk.Match2.MatchFunctions.MatchFunctionListOp
+                .Execute(_Sdk.Namespace);
+            #endregion
             Assert.IsNotNull(response);
         }
     }
--- AccelByte.Sdk.Tests/Services/SessionTests.cs
+++ AccelByte.Sdk.Tests/Services/SessionTests.cs
@@ -3,6 +3,8 @@
 // and restrictions contact your company contract manager.
 
 using System;
+using System.Linq;
+using System.Collections.ObjectModel;
 using System.Collections.Generic;
 using NUnit.Framework;
 
@@ -11,14 +13,218 @@ using AccelByte.Sdk.Api;
 using AccelByte.Sdk.Core.Util;
 
 using AccelByte.Sdk.Api.Session.Model;
-
+using AccelByte.Sdk.Tests.Integration;
 namespace AccelByte.Sdk.Tests.Services
 {
     [TestFixture(Category = "FluentIntegration")]
     [Explicit]
     public class SessionTests : BaseServiceTests
     {
+        private ITestPlayer? _Player1 = null;
+
+        private ITestPlayer? _Player2 = null;
+
         public SessionTests() : base(false) { }
+
+        protected override void OnStartup(AccelByteSDK sdk)
+        {
+            DisableRetry();
+
+            //_Player1 = new ExistingTestPlayer("AB_PLAYER1", true);
+            _Player1 = new NewTestPlayer(sdk, true);
+            //_Player2 = new ExistingTestPlayer("AB_PLAYER2", true);
+            _Player2 = new NewTestPlayer(sdk, true);
+
+            ResetPolicy();
+        }
+
+        protected override void OnBeforeEnd()
+        {
+            _Player1?.Logout();
+            _Player2?.Logout();
+        }
+
+        [Test]
+        public void HealthCheckTests()
+        {
+            Assert.IsNotNull(_Sdk);
+            if (_Sdk == null)
+                return;
+
+            DisableRetry();
+            Assert.DoesNotThrow(() =>
+            {
+                #region Session Health Check
+                _Sdk.Session.Operations.GetHealthcheckInfoV1Op
+                    .Execute();
+                #endregion
+            });
+        }
+
+        [Test]
+        public void ConfigurationTemplateTests()
+        {
+            Assert.IsNotNull(_Sdk);
+            if (_Sdk == null)
+                return;
+
+            DisableRetry();
+
+            string cfgTemplateName = $"csharp_sdk_template_{Helper.GenerateRandomId(4)}";
+
+            #region Create session configuration template
+            ApimodelsCreateConfigurationTemplateRequest cTemplateBody = new ApimodelsCreateConfigurationTemplateRequest()
+            {
+                Name = cfgTemplateName,
+                Type = "P2P",
+                MinPlayers = 2,
+                MaxPlayers = 2,
+                InviteTimeout = 60,
+                InactiveTimeout = 60,
+                Joinability = "OPEN",
+                ClientVersion = "1.0.0",
+                RequestedRegions = new List<string>()
+                {
+                    "us-west-2"
+                }
+            };
+
+            _Sdk.Session.ConfigurationTemplate.AdminCreateConfigurationTemplateV1Op
+                .Execute(cTemplateBody, _Sdk.Namespace);
+
+            ApimodelsConfigurationTemplateResponse? cfgTemplate = _Sdk.Session.ConfigurationTemplate.AdminGetConfigurationTemplateV1Op
+                .Execute(cfgTemplateName, _Sdk.Namespace);
+            #endregion
+            Assert.IsNotNull(cfgTemplate);
+            if (cfgTemplate != null)
+            {
+                Assert.AreEqual("P2P", cfgTemplate.Type!);
+                Assert.AreEqual("OPEN", cfgTemplate.Joinability!);
+                Assert.AreEqual(2, cfgTemplate.MaxPlayers!);
+            }
+
+            #region Update session configuration template
+            ApimodelsUpdateConfigurationTemplateRequest uTemplateBody = new ApimodelsUpdateConfigurationTemplateRequest()
+            {
+                Name = cfgTemplateName,
+                Type = "P2P",
+                Joinability = "OPEN",
+                MaxPlayers = 4
+            };
+
+            ApimodelsConfigurationTemplateResponse? uptTemplate = _Sdk.Session.ConfigurationTemplate.AdminUpdateConfigurationTemplateV1Op
+                .Execute(uTemplateBody, cfgTemplateName, _Sdk.Namespace);
+            #endregion
+            Assert.IsNotNull(uptTemplate);
+            if (uptTemplate != null)
+            {
+                Assert.AreEqual(4, uptTemplate.MaxPlayers!);
+            }
+
+            #region Delete session configuration template
+            ResponseError? response = _Sdk.Session.ConfigurationTemplate.AdminDeleteConfigurationTemplateV1Op
+                .Execute(cfgTemplateName, _Sdk.Namespace);
+            #endregion
+            Assert.IsNull(response);
+
+            ResetPolicy();
+        }
+
+        [Test]
+        public void GameSessionTests()
+        {
+            Assert.IsNotNull(_Sdk);
+            if (_Sdk == null)
+                return;
+
+            Assert.IsNotNull(_Player1);
+            if (_Player1 == null)
+                return;
+
+            Assert.IsNotNull(_Player2);
+            if (_Player2 == null)
+                return;
+
+            DisableRetry();
+
+            string cfgTemplateName = $"csharp_sdk_template_{Helper.GenerateRandomId(4)}";
+            _Sdk.Session.ConfigurationTemplate.AdminCreateConfigurationTemplateV1Op
+                .Execute(new()
+                {
+                    Name = cfgTemplateName,
+                    Type = "P2P",
+                    MinPlayers = 2,
+                    MaxPlayers = 2,
+                    InviteTimeout = 60,
+                    InactiveTimeout = 60,
+                    Joinability = "OPEN",
+                    ClientVersion = "1.0.0",
+                    RequestedRegions = new List<string>()
+                    {
+                        "us-west-2"
+                    }
+                }, _Sdk.Namespace);
+
+            string gameSessionId = String.Empty;
+            _Player1.Run((sdk, player) =>
+            {
+                #region Create a game session
+                ApimodelsCreateGameSessionRequest newGSRequest = new ApimodelsCreateGameSessionRequest()
+                {
+                    ConfigurationName = cfgTemplateName,
+                };
+
+                ApimodelsGameSessionResponse? newGSResponse = sdk.Session.GameSession.CreateGameSessionOp
+                    .Execute(newGSRequest, sdk.Namespace);
+                #endregion
+                Assert.IsNotNull(newGSResponse);
+                if (newGSResponse != null)
+                    gameSessionId = newGSResponse.Id!;
+            });
+
+            if (gameSessionId == String.Empty)
+                Assert.Fail("No game session id");
+
+            _Player2.Run((sdk, player) =>
+            {
+                #region Join a game session
+                ApimodelsGameSessionResponse? p2GsJoin = sdk.Session.GameSession.JoinGameSessionOp
+                    .Execute(sdk.Namespace, gameSessionId);
+                #endregion
+                Assert.IsNotNull(p2GsJoin);
+            });           
+
+            ApimodelsGameSessionResponse? gsCheck = _Sdk.Session.GameSession.GetGameSessionOp
+                .Execute(_Sdk.Namespace, gameSessionId);
+            if (gsCheck != null)
+            {
+                Assert.AreEqual(2, gsCheck.Members!.Count);
+                List<string> userIds = gsCheck.Members!.Select(item => item.Id!).ToList();
+                Assert.Contains(_Player1.UserId, userIds);
+                Assert.Contains(_Player2.UserId, userIds);
+            }
+
+            _Player2.Run((sdk, player) =>
+            {
+                #region Leave a game session
+                sdk.Session.GameSession.LeaveGameSessionOp
+                    .Execute(sdk.Namespace, gameSessionId);
+                #endregion
+            });
+
+            _Player1.Run((sdk, player) =>
+            {
+                #region Delete a game session
+                sdk.Session.GameSession.DeleteGameSessionOp
+                    .Execute(sdk.Namespace, gameSessionId);
+                #endregion
+            });
+
+            _Sdk.Session.ConfigurationTemplate.AdminDeleteConfigurationTemplateV1Op
+                .Execute(cfgTemplateName, _Sdk.Namespace);            
+
+            ResetPolicy();
+        }
 
         [Test]
         public void QueryGameSessions()
@@ -27,9 +233,137 @@ namespace AccelByte.Sdk.Tests.Services
             if (_Sdk == null)
                 return;
 
+            #region Query game sessions
             var response = _Sdk.Session.GameSession.PublicQueryGameSessionsOp
                 .Execute(new Dictionary<string, object>() { }, _Sdk.Namespace);
+            #endregion
             Assert.IsNotNull(response);
+        }
+
+        [Test]
+        public void PartyTests()
+        {
+            Assert.IsNotNull(_Sdk);
+            if (_Sdk == null)
+                return;
+
+            Assert.IsNotNull(_Player1);
+            if (_Player1 == null)
+                return;
+
+            Assert.IsNotNull(_Player2);
+            if (_Player2 == null)
+                return;
+
+            DisableRetry();
+
+            string cfgTemplateName = $"csharp_sdk_template_{Helper.GenerateRandomId(4)}";
+            _Sdk.Session.ConfigurationTemplate.AdminCreateConfigurationTemplateV1Op
+                .Execute(new()
+                {
+                    Name = cfgTemplateName,
+                    Type = "P2P",
+                    MinPlayers = 2,
+                    MaxPlayers = 2,
+                    InviteTimeout = 60,
+                    InactiveTimeout = 60,
+                    Joinability = "OPEN",
+                    ClientVersion = "1.0.0",
+                    RequestedRegions = new List<string>()
+                    {
+                        "us-west-2"
+                    }
+                }, _Sdk.Namespace);
+
+            string partyId = String.Empty;
+            string joinCode = String.Empty;
+            
+            _Player1.Run((sdk, player) =>
+            {
+                #region User create a party
+                ApimodelsCreatePartyRequest partyRequest = new ApimodelsCreatePartyRequest()
+                {
+                    ConfigurationName = cfgTemplateName,
+                    Members = new List<ApimodelsRequestMember>()
+                    {
+                        new ApimodelsRequestMember()
+                        {
+                            ID = player.UserId
+                        }
+                    }
+                };
+
+                ApimodelsPartySessionResponse? partyResponse = sdk.Session.Party.PublicCreatePartyOp
+                    .Execute(partyRequest, sdk.Namespace);
+                #endregion
+                Assert.IsNotNull(partyResponse);
+                if (partyResponse != null)
+                {
+                    partyId = partyResponse.Id!;
+                    joinCode = partyResponse.Code!;
+                }   
+            });
+
+            if (partyId == String.Empty)
+                Assert.Fail("No party id");
+            if (joinCode == String.Empty)
+                Assert.Fail("No join code");
+
+            _Player2.Run((sdk, player) =>
+            {
+                #region User join a party with code
+                ApimodelsJoinByCodeRequest joinRequest = new ApimodelsJoinByCodeRequest()
+                {
+                    Code = joinCode
+                };
+
+                ApimodelsPartySessionResponse? joinResponse = sdk.Session.Party.PublicPartyJoinCodeOp
+                    .Execute(joinRequest, sdk.Namespace);
+                #endregion
+                Assert.IsNotNull(joinResponse);
+            });
+
+            #region Get party detail
+            ApimodelsPartySessionResponse? partyData = _Sdk.Session.Party.PublicGetPartyOp
+                .Execute(_Sdk.Namespace, partyId);
+            #endregion
+            Assert.IsNotNull(partyData);
+            if (partyData != null)
+            {
+                Assert.AreEqual(2, partyData.Members!.Count);
+                List<string> userIds = partyData.Members!.Select(item => item.Id!).ToList();
+                Assert.Contains(_Player1.UserId, userIds);
+                Assert.Contains(_Player2.UserId, userIds);
+            }
+
+            _Player2.Run((sdk, player) =>
+            {
+                #region User leave a party
+                sdk.Session.Party.PublicPartyLeaveOp
+                    .Execute(sdk.Namespace, partyId);
+                #endregion
+            });
+
+            ApimodelsPartySessionResponse? uPartyData = _Sdk.Session.Party.PublicGetPartyOp
+                .Execute(_Sdk.Namespace, partyId);
+            Assert.IsNotNull(uPartyData);
+            if (uPartyData != null)
+            {
+                Assert.AreEqual(2, uPartyData.Members!.Count);
+
+                //Get id of members who are still in party.
+                List<string> userIds = uPartyData.Members!
+                    .Where(item => item.Status! != "LEFT")
+                    .Select(item => item.Id!).ToList();
+
+                Assert.Contains(_Player1.UserId, userIds);
+                Assert.That(userIds, Has.No.Member(_Player2.UserId));
+            }
+
+            _Sdk.Session.ConfigurationTemplate.AdminDeleteConfigurationTemplateV1Op
+                .Execute(cfgTemplateName, _Sdk.Namespace);
+
+            ResetPolicy();
         }
     }
 }
