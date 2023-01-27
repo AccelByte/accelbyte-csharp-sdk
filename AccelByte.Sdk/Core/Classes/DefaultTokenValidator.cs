b705f0b5278e71e831298a9c829da892962f3304
Rinardi Sarean <rinardi@accelbyte.net>
2023-01-27 10:03:10 +0700

    feat: implement default and refactor local token validation

---
+++ AccelByte.Sdk/Core/Classes/DefaultTokenValidator.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (c) 2023 AccelByte Inc. All Rights Reserved.
+// This is licensed software from AccelByte Inc, for limitations
+// and restrictions contact your company contract manager.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AccelByte.Sdk.Api;
+using AccelByte.Sdk.Api.Iam.Model;
+
+namespace AccelByte.Sdk.Core
+{
+    public class DefaultTokenValidator : TokenValidator, ITokenValidator
+    {
+        public bool Validate(AccelByteSDK sdk, string accessToken)
+        {
+            try
+            {
+                OauthmodelTokenResponseV3? response = sdk.Iam.OAuth20.VerifyTokenV3Op.Execute(accessToken);
+                if (response == null)
+                    throw new Exception("Server did not response to token validation request");
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Validate(AccelByteSDK sdk, string accessToken, string permission, int action)
+        {
+            try
+            {
+                OauthmodelTokenResponseV3? response = sdk.Iam.OAuth20.VerifyTokenV3Op.Execute(accessToken);
+                if (response == null)
+                    throw new Exception("Server did not response to token validation request");
+
+                if (response.Permissions == null)
+                    return false;
+
+                bool foundMatchingPermission = false;
+                foreach (var p in response.Permissions)
+                {
+                    if ((p.Resource != null) && (p.Action != null))
+                    {
+                        if (IsResourceAllowed(p.Resource, permission))
+                        {
+                            if (PermissionAction.Has(p.Action.Value, action))
+                            {
+                                foundMatchingPermission = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                if (!foundMatchingPermission)
+                    return false;
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ AccelByte.Sdk/Core/Classes/TokenValidator.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) 2023 AccelByte Inc. All Rights Reserved.
+// This is licensed software from AccelByte Inc, for limitations
+// and restrictions contact your company contract manager.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccelByte.Sdk.Core
+{
+    public abstract class TokenValidator
+    {
+        protected bool IsResourceAllowed(string accessPermissionResource, string requiredPermissionResource)
+        {
+            string[] requiredPermResSections = requiredPermissionResource.Split(':');
+            int requiredPermResSectionLen = requiredPermResSections.Length;
+            string[] accessPermResSections = accessPermissionResource.Split(':');
+            int accessPermResSectionLen = accessPermResSections.Length;
+
+            int minSectionLen = accessPermResSectionLen;
+
+            if (minSectionLen > requiredPermResSectionLen)
+                minSectionLen = requiredPermResSectionLen;
+
+            for (int i = 0; i < minSectionLen; i++)
+            {
+                string userSection = accessPermResSections[i];
+                string requiredSection = requiredPermResSections[i];
+
+
+                if ((userSection != requiredSection) && (userSection != "*"))
+                    return false;
+            }
+
+            if (accessPermResSectionLen == requiredPermResSectionLen)
+                return true;
+
+            if (accessPermResSectionLen < requiredPermResSectionLen)
+            {
+                if (accessPermResSections[accessPermResSectionLen - 1] == "*")
+                {
+                    if (accessPermResSectionLen < 2)
+                        return true;
+
+                    string segment = accessPermResSections[accessPermResSectionLen - 2];
+
+                    if ((segment == "NAMESPACE") || (segment == "USER"))
+                        return false;
+
+                    return true;
+                }
+
+                return false;
+            }
+
+            for (int i = requiredPermResSectionLen; i < accessPermResSectionLen; i++)
+            {
+                if (accessPermResSections[i] != "*")
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
